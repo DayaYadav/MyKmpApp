27cff19eeea687d55e993c21591c2a20c924aa8e
dayayadav <daya20oct@gmail.com>
2026-06-24 14:22:51 +0530

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/dev/mykmpapp/Greeting.kt
@@ -0,0 +1,9 @@
+package com.dev.mykmpapp
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return sayHello(platform.name)
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/dev/mykmpapp/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.dev.mykmpapp
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
