3fd38b5e0e20aed512d3af412eee87baeae273cd
dayayadav <daya20oct@gmail.com>
2026-06-24 16:18:19 +0530

    chore: update ui

--- shared/src/commonMain/kotlin/com/dev/mykmpapp/App.kt
+++ shared/src/commonMain/kotlin/com/dev/mykmpapp/App.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,6 +17,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
 import mykmpapp.shared.generated.resources.Res
@@ -32,7 +36,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me!Daya")
+                Text("Click me! Daya")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
@@ -44,6 +48,24 @@ fun App() {
                     Text("Compose: $greeting")
                 }
             }
+            //
+            if(showContent)
+                NumberList()
+        }
+    }
+}
+
+@Composable
+fun NumberList() {
+    val numbers = (1 until 20).toList()
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(numbers) { number ->
+            Text(
+                text = number.toString(),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
